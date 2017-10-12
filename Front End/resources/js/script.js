632cfa910bacbd525b8eafa77c45b6986d59b67b
Linh Phan <linhphanhust@gmail.com>
2017-10-12 11:19:48 +0700

    Fix signup/login tab, add bidHistory

--- Front End/resources/js/script.js
+++ Front End/resources/js/script.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
  
-    $('input:radio[name="accountType"]').change(function() { 
-        if($(this).val()=='business') {
-            $('#signupBusiness').removeClass('hide') ;
-            $('#signupPersonal').addClass('hide') ;
-        }
-        else {
-            $('#signupBusiness').addClass('hide') ;
-            $('#signupPersonal').removeClass('hide') ;
-        }
-    });
+    
     $('ul.selectCategoryMenu li').click(function(e){
         $('ul.selectCategoryMenu li').removeClass('active');
         $(this).addClass('active');
